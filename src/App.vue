8b9418fbd2ab9a12dcc7adc7149a793f401f9db4
MarioBroi <inoix16@gmail.com>
2024-05-30 14:45:50 +0100

    add API call for Projects data and check the result with console log

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,37 @@
 <script>
+
+import axios from 'axios';
+
+export default {
+  name: 'App',
+  data() {
+
+    return {
+      base_api_url: 'http://127.0.0.1:8000',
+      base_projects_url: '/api/projects',
+      projects: [],
+      loading: true,
+    }
+  },
+  methods: {
+    callApi(url) {
+      axios
+        .get(url)
+        .then(response => {
+          console.log(response);
+          this.projects = response.data.projects
+          this.loading = false
+        })
+        .catch(err => {
+          console.log(err);
+        })
+    }
+  },
+  mounted() {
+    let url = this.base_api_url + this.base_projects_url
+    this.callApi(url);
+  }
+}
 </script>
 
 <template>
