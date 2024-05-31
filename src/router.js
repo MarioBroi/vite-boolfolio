bde08412f72783c60cdcfcb91deea8da714a9c2c
MarioBroi <inoix16@gmail.com>
2024-05-31 15:37:56 +0100

    add routes for all the new pages

--- src/router.js
+++ src/router.js
@@ -1,5 +1,11 @@
 import { createWebHistory, createRouter } from 'vue-router'
-import { AppHome } from "./views/AppHome.vue"
+
+import AppHome from "./views/AppHome.vue";
+import AppAbout from "./views/AppAbout.vue";
+import AppSkill from "./views/AppSkill.vue";
+import AppProjects from "./views/AppProjects.vue";
+import AppEducation from "./views/AppEducation.vue";
+import AppContacts from "./views/AppContacts.vue";
 
 const router = createRouter({
     history: createWebHistory(),
@@ -7,7 +13,32 @@ const router = createRouter({
         {
             path: '/',
             name: 'home',
-            component: AppHome,
+            component: AppHome
+        },
+        {
+            path: '/about',
+            name: 'about',
+            component: AppAbout,
+        },
+        {
+            path: '/skill',
+            name: 'skill',
+            component: AppSkill,
+        },
+        {
+            path: '/projects',
+            name: 'projects',
+            component: AppProjects,
+        },
+        {
+            path: '/education',
+            name: 'education',
+            component: AppEducation,
+        },
+        {
+            path: '/contacts',
+            name: 'contacts',
+            component: AppContacts,
         },
     ]
 })
