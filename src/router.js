7d080d35d6f3763f48649ba778578f718379faa0
MarioBroi <inoix16@gmail.com>
2024-05-31 14:46:28 +0100

    add AppHome page with route

--- src/router.js
+++ src/router.js
@@ -1,13 +1,14 @@
 import { createWebHistory, createRouter } from 'vue-router'
+import { AppHome } from "./views/AppHome.vue"
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
         {
-            path: '',
-            name: '',
-            component: '',
-        }
+            path: '/',
+            name: 'home',
+            component: AppHome,
+        },
     ]
 })
 
