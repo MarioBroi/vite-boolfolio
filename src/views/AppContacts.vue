7289ba08df2ded8046f051f6153480ea5f4ac997
MarioBroi <inoix16@gmail.com>
2024-06-11 09:36:25 +0200

    edit contacts page button and input style

--- src/views/AppContacts.vue
+++ src/views/AppContacts.vue
@@ -73,29 +73,39 @@ export default {
 
         <div class="mb-3">
             <label for="name" class="form-label">Name</label>
-            <input type="text" class="form-control" :class="{ 'border border-danger': errors.name }" name="name"
-                id="name" aria-describedby="helpId" placeholder="..." v-model="name" />
+            <input type="text" class="form-control input_form" :class="{ 'border border-danger': errors.name }"
+                name="name" id="name" aria-describedby="helpId" placeholder="..." v-model="name" />
             <small id="helpId" class="form-text text-light">Type your name</small>
         </div>
         <!-- /.name -->
 
         <div class="mb-3">
             <label for="email" class="form-label">Email</label>
-            <input type="email" class="form-control" :class="{ 'border border-danger': errors.email }" name="email"
-                id="email" aria-describedby="emailHelpId" placeholder="abc@mail.com" v-model="email" />
+            <input type="email" class="form-control input_form" :class="{ 'border border-danger': errors.email }"
+                name="email" id="email" aria-describedby="emailHelpId" placeholder="abc@mail.com" v-model="email" />
             <small id="emailHelpId" class="form-text text-light">Type your email</small>
         </div>
         <!-- /.email -->
 
         <div class="mb-3">
-            <label for="message" class="form-label">message</label>
-            <textarea class="form-control" :class="{ 'border border-danger': errors.message }" name="message"
+            <label for="message" class="form-label">Message</label>
+            <textarea class="form-control input_form" :class="{ 'border border-danger': errors.message }" name="message"
                 id="message" rows="3" v-model="message"></textarea>
         </div>
         <!-- /.message -->
 
-        <button type="submit" class="btn btn-primary" :disabled="loading">
-            {{ loading ? 'Sending...' : 'Send' }}
+        <button type="submit" class="btn submit_button" :disabled="loading">
+            <!-- {{ loading ? `'Sending...'` : 'Send' }} -->
+            <div v-if="loading == true">
+                <div class="looping-rhombuses-spinner">
+                    <div class="rhombus"></div>
+                    <div class="rhombus"></div>
+                    <div class="rhombus"></div>
+                </div>
+            </div>
+            <div v-else>
+                Send
+            </div>
         </button>
 
     </form>
